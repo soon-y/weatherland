825e5a9571d57f801355486a9ca71e4264362db7
Soonyoung Park <soon.syp@gmail.com>
2026-05-30 18:33:41 +0200

    update animation

--- src/components/icons/moon.js
+++ src/components/icons/moon.js
@@ -9,35 +9,35 @@ export function Moon({cloud = false}) {
       width="32"
       height="32"
     >
-      <path className="scale-90 -translate-x-[1px] translate-y-[1px] origin-center"
+      <path className="scale-90 -translate-x-[2px] translate-y-[1px] origin-center"
         d="m14.3 6c-5.8 1.3-9.4 7-8.1 12.8 1.3 5.8 7.1 9.4 12.8 8.1 2.9-0.7 5.2-2.4 6.6-4.6 0.2-0.3 0.2-0.7 0-0.9-0.2-0.3-0.6-0.4-0.9-0.3q-0.6 0.3-1.4 0.5c-4.5 1-9-1.8-10-6.4-0.7-3.1 0.3-6.1 2.5-8.1 0.3-0.2 0.3-0.6 0.2-0.9-0.1-0.3-0.5-0.5-0.8-0.4q-0.5 0.1-0.9 0.2z"
         style={fillStyle}
       />
 
       {!cloud && 
-      <g className="animate-[ping_4s_linear_infinite] origin-center translate-x-[8px] -translate-y-[8px]">
+      <g className="animate-[pulse_4s_linear_infinite]">
         <path
           fillRule="evenodd"
-          d="m17 16l-1 5-1-5 1-5z"
+          d="m22 8l-1 5-1-5 1-5z"
           style={fillStyle}
         />
         <path
           fillRule="evenodd"
-          d="m16 17l-5-1 5-1 5 1z"
+          d="m21 9l-5-1 5-1 5 1z"
           style={fillStyle}
         />
       </g>}
 
       {!cloud && 
-      <g className="animate-[ping_4s_linear_infinite] origin-center scale-50 translate-x-[12px] translate-y-[2px]">
+      <g className="animate-[pulse_2s_linear_infinite]">
         <path
           fillRule="evenodd"
-          d="m17 16l-1 5-1-5 1-5z"
+          d="m26.8 17l-0.8 4-0.8-4 0.8-4z"
           style={fillStyle}
         />
         <path
           fillRule="evenodd"
-          d="m16 17l-5-1 5-1 5 1z"
+          d="m26 17.8l-4-0.8 4-0.8 4 0.8z"
           style={fillStyle}
         />
       </g>}
