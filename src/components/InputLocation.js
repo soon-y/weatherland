df97f9233389b121afad3b6f6d48254d134a3c1b
Soonyoung Park <soon.syp@gmail.com>
2026-06-06 21:01:15 +0200

    update margin

--- src/components/InputLocation.js
+++ src/components/InputLocation.js
@@ -90,13 +90,13 @@ export default function InputArea({ setGeolocation, hide }) {
       {latitudeVal != null && longitudeVal != null && suburbArea && postcodeVal &&
         <div>
           {!hide ?
-            <div className='p-4'>
+            <div className='p-6'>
               <div className={'font-bold text-xl flex bg-white/40 backdrop-blur-lg py-1 px-4 rounded-full'}>
                 {suburbArea}
               </div>
             </div>
             :
-            <div className='p-4 grid place-items-end gap-2'>
+            <div className='p-6 grid place-items-end gap-2'>
               <Input
                 label={"Region"}
                 value={suburbArea}
