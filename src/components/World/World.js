7171aaef16694470dd7cd66f81cbbb00a6ae0cea
Soonyoung Park <soon.syp@gmail.com>
2026-06-07 12:03:45 +0200

    adjust camera position depending on  screen size

--- src/components/World/World.js
+++ src/components/World/World.js
@@ -1,5 +1,5 @@
 import { OrbitControls } from "@react-three/drei"
-import { Canvas } from "@react-three/fiber"
+import { Canvas, useThree } from "@react-three/fiber"
 import { levaStore } from 'leva'
 import { param, useIsDebug } from "@/lib/param"
 import DebugUI from "../debugUI"
@@ -40,12 +40,48 @@ function World({ hourly, daily, index }) {
         dampingFactor={0.03}
       />
 
+      <CameraController />
+
       <group position={param.worldPos}>
         <Environment store={levaStore} hourly={hourly} daily={daily} index={index} indexD={indexD} />
         <WorldGround />
       </group>
     </Canvas>
   </>
+}
+
+function CameraController() {
+  const { camera } = useThree()
+
+  useEffect(() => {
+    const handleResize = () => {
+      const isMobile = window.innerWidth < 768
+
+      if (isMobile) {
+        const width = window.innerWidth
+        const scale = Math.max(1, 600 / width)
+
+        camera.position.set(
+          param.camPos[0] * scale,
+          param.camPos[1] * scale,
+          param.camPos[2] * scale,)
+      } else {
+        camera.position.set(...param.camPos)
+      }
+
+      camera.updateProjectionMatrix()
+    }
+
+    handleResize()
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [camera])
+
+  return null
 }
 
 export default World
