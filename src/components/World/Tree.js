cfbbc5f1ed334f963b7b06c916493244b9addede
Soonyoung Park <soon.syp@gmail.com>
2026-06-13 23:49:49 +0200

    add snowDepth

--- src/components/World/Tree.js
+++ src/components/World/Tree.js
@@ -1,3 +1,4 @@
+import * as THREE from 'three'
 import { useEffect, useMemo, useRef } from "react"
 import { useFrame } from "@react-three/fiber"
 import { param } from "@/lib/param"
@@ -6,12 +7,13 @@ import vertexShader from './shader/tree/vertexShader.glsl'
 import fragmentShader from './shader/tree/fragmentShader.glsl'
 import { useGLTF } from '@react-three/drei'
 
-export default function Tree({ progress, windDir, windSpd }) {
+export default function Tree({ progress, windDir, windSpd, snowDepth }) {
   const { nodes, materials } = useGLTF('models/tree.glb')
 
   const leafMaterialRef = useRef()
   const trunkMaterial = useMemo(() => materials.trunk.clone(), [materials])
   const trunkShaderRef = useRef()
+  const snowRef = useRef()
 
   const geometry = nodes.Cube.geometry
   geometry.setAttribute(
@@ -24,10 +26,26 @@ export default function Tree({ progress, windDir, windSpd }) {
     uProgress: { value: progress },
     uWindSpeed: { value: 0 },
     uWindDir: { value: 0 },
+    uSnowDepth: { value: 0 }
   }), [])
 
   useFrame((state) => {
     const time = state.clock.elapsedTime
+
+    if (!snowRef.current) return
+
+    const targetPosY = THREE.MathUtils.clamp(
+      THREE.MathUtils.mapLinear(
+        snowDepth, 0, 0.5, -0.4, -0.199
+      ), -0.4, -0.199
+    )
+
+    snowRef.current.position.y =
+      THREE.MathUtils.lerp(
+        snowRef.current.position.y,
+        targetPosY,
+        0.05
+      )
 
     if (trunkShaderRef.current) {
       trunkShaderRef.current.uniforms.uTime.value = time
@@ -40,6 +58,7 @@ export default function Tree({ progress, windDir, windSpd }) {
       leafMaterialRef.current.uniforms.uProgress.value = progress
       leafMaterialRef.current.uniforms.uWindSpeed.value = windSpd.current
       leafMaterialRef.current.uniforms.uWindDir.value = windDir.current
+      leafMaterialRef.current.uniforms.uSnowDepth.value = snowDepth
     }
   })
 
@@ -78,6 +97,16 @@ export default function Tree({ progress, windDir, windSpd }) {
   return (
     <group position={param.groundPos}>
       <group position={[-4.7, 0, -4]} scale={1.2} rotation-y={0.2}>
+        <mesh
+          ref={snowRef}
+          castShadow
+          receiveShadow
+          geometry={nodes.snowTrunk.geometry}
+          material={materials.snow}
+          position={[0, -0.199, 0]}
+          visible={snowDepth >= 0.1}
+        />
+
         <group position={[0, -0.199, 0]}>
           <mesh castShadow receiveShadow
             geometry={nodes.Cube.geometry}
