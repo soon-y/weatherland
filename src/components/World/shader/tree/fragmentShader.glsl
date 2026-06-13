78a4bea9c0855d7491c7d46f491196ed6065fba2
Soonyoung Park <soon.syp@gmail.com>
2026-06-13 23:51:09 +0200

    change leaves color depending on snowDepth

--- src/components/World/shader/tree/fragmentShader.glsl
+++ src/components/World/shader/tree/fragmentShader.glsl
@@ -1,4 +1,5 @@
 uniform float uProgress;
+uniform float uSnowDepth;
 
 varying vec3 vNormal;
 
@@ -41,20 +42,25 @@ void main() {
     vec3 lightDir = normalize(vec3(0.3, 1.0, 0.2));
 
     float ndl = dot(normal, lightDir);
-
     ndl = ndl * 0.5 + 0.5;
 
     float shading = mix(0.35, 1.0, ndl);
 
     vec3 leafColor = vec3(0.42, 0.72, 0.05);
-
     leafColor *= shading;
+
+    vec3 snowColor = vec3(1.0);
+    snowColor *= shading;
+
+    float snowAmount = smoothstep(0.1, 0.5, uSnowDepth);
+
+    vec3 surfaceColor = mix(leafColor, snowColor, snowAmount);
 
     vec3 nightColor = vec3(0.01, 0.02, 0.015);
 
     float brightness = getBrightness(uProgress);
 
-    vec3 color = mix(nightColor, leafColor, brightness);
+    vec3 color = mix(nightColor, surfaceColor, brightness);
 
     gl_FragColor = vec4(color, 1.0);
 }
