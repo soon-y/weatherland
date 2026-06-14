92223a4d3634c8c3d35adbacf53a37645b93b7ad
Soonyoung Park <soon.syp@gmail.com>
2026-06-14 11:56:28 +0200

    delete invalid values and display it only if it is valid

--- src/components/weatherGraph/dailyMoon.js
+++ src/components/weatherGraph/dailyMoon.js
@@ -3,8 +3,6 @@ import MoonImg from "./graphBox/moonBox"
 export default function DailyMoon({ indexW, moon }) {
   const moonrise = moon[indexW].moonrise
   const moonset = moon[indexW].moonset
-  const nightBegin = moon[indexW].night_begin
-  const nightEnd = moon[indexW].night_end
   const phaseAngle = moon[indexW].phaseData.moonphase
   const moonPhase = moon[indexW].moon_phase.replace('_', ' ')
 
@@ -12,23 +10,13 @@ export default function DailyMoon({ indexW, moon }) {
     <div className="w-full">
       <div className="pt-8 pb-4">
         <div className="flex justify-between">
-          <div className="grid grid-cols-[80px_1fr]">
-            <span>Moonrise</span>
-            <span>Moonset</span>
-          </div>
-          <span>Night</span>
+          {!moonrise.includes('-:-') && <span>Moonrise</span>}
+          {!moonset.includes('-:-') && <span>Moonset</span>}
         </div>
 
         <div className="text-2xl flex justify-between items-end">
-          <div className="grid grid-cols-[80px_1fr]">
-            <span>{moonrise}</span>
-            <span>{moonset}</span>
-          </div>
-
-          <span className="text-xl">{nightBegin}
-            <span className="text-base"> to </span>
-            {nightEnd}
-          </span>
+          {!moonrise.includes('-:-') && <span>{moonrise}</span>}
+          {!moonset.includes('-:-') && <span>{moonset}</span>}
         </div>
 
         <div className="p-12">
