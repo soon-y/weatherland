16b718fbf05a4dfff6535377b275eb1364496c9a
Soonyoung Park <soon.syp@gmail.com>
2026-06-26 23:44:06 +0200

    update wrapper size to dynamic

--- src/components/weatherDetails.js
+++ src/components/weatherDetails.js
@@ -21,7 +21,7 @@ export default function WeatherDetails({ open, hourly, daily, air, moon, index, 
   const [boxClicked, setBoxClicked] = useState(false)
 
   return (
-    <div className='relative h-full'>
+    <div className='relative w-dvw h-dvh'>
       <div className={`scrollbar-hide ${param.weatherBoxheight} ${param.weatherBoxStyles} ${open ? 'opacity-100' : 'opacity-0'}
         flex flex-wrap content-start items-start overflow-scroll gap-3 p-2 pt-[1px] sm:p-4 pb-12 duration-500
         `}>
